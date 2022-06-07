ef3faac48f76e1857bd55a63d86cd446ffdf3f5d
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-06-07 18:30:43 +0300

    Creating api to signup as a user

--- Brisk-BackEnd/app/Http/Controllers/UserController.php
+++ Brisk-BackEnd/app/Http/Controllers/UserController.php
@@ -16,4 +16,29 @@ class UserController extends Controller
         ], 200);
 
     }
+
+    public function signUp(Request $request){
+
+        $user = new User;
+        
+        $user->first_name = $request->first_name;
+        $user->last_name = $request->last_name;
+        $user->username = $request->username;
+        $user->password = hash("sha256", $request->password);
+        $user->dob = $request->dob;
+        $user->country = $request->country;
+        $user->city = $request->city;
+        $user->phone = $request->phone;
+        $user->gender = $request->gender;
+        $user->is_admin = "0";
+
+        $user->save();
+
+        return response()->json([
+            "status" => "Success",
+        ], 200);
+
+    }
+
+
 }
--- Brisk-BackEnd/routes/api.php
+++ Brisk-BackEnd/routes/api.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/all_users',[UserController::class, 'getAllUsers'])->name("getAllUsers");
+Route::post('/signup',[UserController::class, 'signUp'])->name("signUp");
 
